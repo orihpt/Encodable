3f8f8556d5c279008553bd5375c398e385a56fbf
orihpt <ori9070.hpt@gmail.com>
2020-11-09 09:14:56 +0200

    Add marks

--- Encodable/Code/Person.swift
+++ Encodable/Code/Person.swift
@@ -11,10 +11,14 @@ struct Person: Encodable, Decodable {
     var firstName: String
     var lastName: String
     var birthday: Date
+//    var favorite: Bool = false
+    
+    // Try to comment these lines, save, uncomment these lines, and load. I want a fix to that error.
     
     init() {
         self.firstName = "Tim"
         self.lastName = "Cook"
         self.birthday = Date()
+//        self.favorite = false
     }
 }
